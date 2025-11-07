54a3e2d789b044132be47bac5ae6b1e1defef0dd
DevDilip28 <dilipasdeo028@gmail.com>
2025-11-07 19:58:10 +0530

    fix baseURL

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL =
   import.meta.env.MODE === "development"
     ? "http://localhost:8080/api/v1"
-    : `${import.meta.env.VITE_API_URL}/api/v1`;
+    : "https://algoholic-backend.onrender.com/api/v1";
 
 export const axiosInstance = axios.create({
   baseURL,
