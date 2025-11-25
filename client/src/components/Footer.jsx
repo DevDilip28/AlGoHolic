8da9acbd9eb667787f3bb32ae423a8224d7eb6b2
DevDilip28 <dilipasdeo028@gmail.com>
2025-11-25 19:21:14 +0530

    fix

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -41,13 +41,6 @@ const Footer = () => {
             >
               <Linkedin className="w-5 h-5" />
             </a>
-            <a
-              href="mailto:dilipasdeo4@gmail.com"
-              aria-label="Email"
-              className="hover:text-indigo-400 transition-colors duration-200"
-            >
-              <Mail className="w-5 h-5" />
-            </a>
           </div>
         </div>
       </div>
